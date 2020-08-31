f185cd7fcb542234ad108910d32e5deab0637933
rawan <rawanabodiak12@gmail.com>
2020-08-31 12:19:33 +0300

    changes when use jquery validation

--- js/log.js
+++ js/log.js
@@ -1,39 +1,79 @@
-function validateForm() {
-
-    var email = document.getElementById('exampleInputEmail1').value;
-    var pass = document.getElementById('exampleInputPassword1').value;
-
-    //var rgx =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-    var rgx = /^([a-zA_Z0-9\.-]+)\@([a-z0-9]+)\.([a-z]{2,8})(.[a-z]{2,8})?$/;
-    // var rgx=  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    //var rgx = /\S+@\S+\.\S+/;
-    var valid_email = rgx.test(email);
-
-    if (email == '') {
-        document.getElementById("invalid_name1").innerHTML = "you need to enter your email address";
-
-    }
-    if (pass == '') {
-        document.getElementById("invalid_name2").innerHTML = "you need to enter your password";
-        return false
-    }
-    if (!valid_email) {
-
-        document.getElementById("invalid_name1").innerHTML = "invalid email<br>  remember that email should contain @ and valid domain name ";
-        return false;
-    } else {
+jQuery.validator.addMethod("passwordCheck",
+    function(value, element, param) {
+        if (this.optional(element)) {
+            return true;
+        } else if (!/[A-Z]/.test(value)) {
+            return false;
+        } else if (!/[a-z]/.test(value)) {
+            return false;
+        } else if (!/[0-9]/.test(value)) {
+            return false;
+        }
 
         return true;
+    },
+);
+
+$("#log_form").validate({
+
+    rules: {
+        email: {
+            required: true,
+            email: true,
+        },
+        password: {
+            required: true,
+            minlength: 5,
+            passwordCheck: true
+        },
+    },
+
+    messages: {
+        //name not id
+        email: {
+            required: "Email is required",
+            email: "email should contain @ and valid domain name",
+        },
+        password: {
+            required: "Password is required",
+            minlength: "password length atleast is 5",
+            passwordCheck: "password At least contain 1 uppercase,lowercase 1digit"
+        },
+
+    },
+    submitHandler: function(log_form) {
+
+
+        $.ajax({
+            method: "post",
+            url: "../backend/checklogin.php",
+            data: {
+                email: $('#exampleInputEmail1').val(),
+                password: $('#exampleInputPassword1').val()
+            },
+
+            success: function(data, status, xhr) {
+
+
+                if (data == 1) {
+                    $('#note').show();
+                    $('#note').html("invalid email or password");
+                } else {
+
+                    window.location.href = "profile.html";
+
+                }
+            },
+            error: function(xhr, status, error) {
+                alert("eror in response");
+                console.log(error);
+            },
+        });
     }
 
 
-};
+});
 
-function hide() {
-    document.getElementById("invalid_name1").innerHTML = "";
-    document.getElementById("invalid_name2").innerHTML = "";
-}
 
 function showpass() {
     var password_element = document.getElementById("exampleInputPassword1");
--- js/signup.js
+++ js/signup.js
@@ -1,119 +1,145 @@
-var tmp = 0;
-var myfphone;
+jQuery.validator.addMethod("passwordCheck",
+    function(value, element, param) {
+        if (this.optional(element)) {
+            return true;
+        } else if (!/[A-Z]/.test(value)) {
+            return false;
+        } else if (!/[a-z]/.test(value)) {
+            return false;
+        } else if (!/[0-9]/.test(value)) {
+            return false;
+        }
 
-function formatPhoneNumber(phoneNumberString) {
-    var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
-    var match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/)
-    if (match) {
-        tmp = 0;
-        var intlCode = (match[1] ? '+1 ' : '')
-        return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
+        return true;
+    },
+);
+
+jQuery.validator.addMethod("phoneUS", function(phone_number, element) {
+    phone_number = phone_number.replace(/\s+/g, "");
+    return this.optional(element) || phone_number.length > 9 &&
+        phone_number.match(/^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
+}, "Please enter a valid phone number");
+
+
+$("#sign_form").validate({
+
+    rules: {
+        first: {
+            required: true,
+            number: false,
+        },
+        last: {
+            required: true,
+        },
+        birthday: {
+            required: true,
+            date: true
+        },
+        gender: {
+            required: true,
+        },
+        email: {
+            required: true,
+            email: true,
+        },
+
+        phone: {
+            required: true,
+            phoneUS: true
+
+        },
+        password: {
+            required: true,
+            minlength: 5,
+            passwordCheck: true
+        },
+        confirm: {
+            required: true,
+            equalTo: "#password1",
+        },
+    },
+
+    messages: {
+        //names not id
+        first: {
+            required: "First name is required",
+            number: "the name shouldn't contains numbers"
+        },
+        last: {
+            required: "Last name is required"
+        },
+        birthday: {
+            required: "Birthday is required",
+            date: "please enter valid date"
+        },
+        gender: {
+            required: "Gender is required",
+        },
+        email: {
+            required: "Email is required",
+            email: "email should contain @ and valid domain name",
+        },
+
+        phone: {
+            required: "Phone is required",
+
+        },
+        password: {
+            required: "Password is required",
+            minlength: "password length atleast is 5",
+            passwordCheck: "password At least contain 1 uppercase,lowercase 1digit"
+        },
+        confirm: {
+            required: "Confirm Password is required",
+            equalTo: "please enter the same password ",
+        },
+    },
+    submitHandler: function(sign_form) {
+
+        $('#sucesse_message').html("");
+
+        $('#erorr_message').html("");
+
+        $.ajax({
+            method: "post",
+            url: "../backend/checksign.php",
+            data: {
+                first: $('#first_name').val(),
+                last: $('#last_name').val(),
+                birthday: $('#birthday').val(),
+                Gender: $('#Gender').val(),
+                Email1: $('#Email1').val(),
+                password: $('#password1').val(),
+                confirm: $('#confirm').val(),
+                phone: $('#phone').val(),
+            },
+
+            success: function(data, status, xhr) {
+
+                var obj = JSON.parse(data);
+                $('#sucesse_message').html("");
+
+                $('#erorr_message').html("");
+
+                var error = obj.error;
+                var success = obj.success;
+                $('#sucesse_message').html(success);
+
+                $('#erorr_message').html(error);
+            },
+            error: function(xhr, status, err) {
+                alert("eror in response");
+                console.log(err);
+            },
+        });
+
+
+
+        ;
     }
-    tmp = 1;
-    document.getElementById('invalid_phone').innerHTML = "Wrong Phone";
-    return null;
-}
-
-function validateForm() {
-    var tmp = 0;
-    var first_name = document.getElementById('first-name').value;
-    var last_name = document.getElementById('last-name').value;
-    var birthday = document.getElementById('birthday').value;
-    var genderw = document.getElementById('Gender').value;
-    var email1 = document.getElementById('Email1').value;
-    var phone = document.getElementById('phone').value;
-    var password = document.getElementById('password1').value;
-    var confipass = document.getElementById('confirm').value;
-
-    var rgx = /^([a-zA_Z0-9\.-]+)@([a-z0-9]+)\.([a-z]{2,8})(.[a-z]{2,8})?$/;
-    var x = rgx.test(email1);
-    var res;
-    var f = /^(05)[0-9]{8}$/;
-    var connum = /^[0-9]/;
-    if (last_name === '') {
-
-        document.getElementById("invalid_name2").style.visibility = "visible";
-        tmp++;
-    }
-    if (first_name === '') {
-        document.getElementById("invalid_name1").style.visibility = "visible";
-        tmp++;
-    }
+})
 
 
-
-    if (birthday === '') {
-        document.getElementById("invalid_birt").style.visibility = "visible";
-        tmp++;
-    }
-    if (genderw === '') {
-        document.getElementById("invalid_gender").style.visibility = "visible";
-        tmp++;
-    }
-
-    if (phone === '') {
-        document.getElementById("invalid_phone").style.visibility = "visible";
-        tmp++;
-    }
-    if (password === '') {
-        document.getElementById("invalid_pass").style.visibility = "visible";
-        tmp++;
-    }
-    if (confipass === '') {
-        document.getElementById("invalid_pass2").style.visibility = "visible";
-        tmp++;
-    }
-    if (!x) {
-
-        document.getElementById("invalid_email").innerHTML = "email should contain @ and valid domain name ";
-        tmp++;
-    }
-    if (email1 === '') {
-        document.getElementById("invalid_email").style.visibility = "visible";
-        tmp++;
-    }
-
-    if (password !== confipass) {
-        document.getElementById("invalid_pass2").innerHTML = "no match ";
-        document.getElementById("invalid_pass").innerHTML = "no match ";
-        tmp++;
-
-    }
-
-    if (connum.test(first_name)) {
-        tmp++;
-        document.getElementById("invalid_name1").innerHTML = "name shouldnt contain numbers";
-    }
-
-    /*   if (  (password.match(/[a-z]/g)) && (password.match(
-           /[A-Z]/g)) && (password.match(
-           /[0-9]/g))   && (password.length >= 8))
-       {   res = "TRUE";}
-       else
-       {   res = "FALSE";
-
-           tmp12=1;
-           document.getElementById("invalid_pass").innerHTML=  "password At least contain 1 uppercase,lowercase 1digit,Minimum length 8 " ;
-
-       }*/
-    document.getElementById('phone').value = formatPhoneNumber(phone); // => "+1 (234) 567-8900"
-    myfphone = formatPhoneNumber(phone);
-
-    if (tmp != 0) {
-
-        var b = false;
-        return b;
-    }
-    var b = true
-    return b;
-
-
-}
-
-function hide_comment(elem) {
-
-    elem.style.visibility = "hidden";
-}
 jQuery(document).ready(function() {
     $("#birthday").datepicker({
         changeYear: true,
@@ -129,47 +155,4 @@ jQuery(document).ready(function() {
 
         showAnim: "fold"
     })
-});
-
-$('.sign_btn').click(function() {
-    var valid = validateForm();
-    $('#sucesse_message').html("");
-
-    $('#erorr_message').html("");
-    if (valid) {
-
-        $.ajax({
-            method: "post",
-            url: "../backend/checksign.php",
-            data: {
-                first: $('#first-name').val(),
-                last: $('#last-name').val(),
-                birthday: $('#birthday').val(),
-                Gender: $('#Gender').val(),
-                Email1: $('#Email1').val(),
-                password: $('#password1').val(),
-                confirm: $('#confirm').val(),
-                phone: myfphone
-            },
-
-            success: function(data, status, xhr) {
-
-                var obj = JSON.parse(data);
-
-
-                var error = obj.error;
-                var success = obj.success;
-                $('#sucesse_message').html(success);
-
-                $('#erorr_message').html(error);
-
-            },
-            error: function(xhr, status, err) {
-                alert("eror in response");
-                console.log(err);
-            },
-        });
-    }
-
-
 });
